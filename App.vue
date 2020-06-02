a44506e5f7a6329b569b3b0f125f0a20b985808f
李建敏 <lijianmin1993@gmail.com>
2020-06-02 17:06:41 +0800

    init

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Draw :pointList="pointList" @pointClick="pointClick" @add="handleAdd"></Draw>
+  </div>
+</template>
+<script>
+import Draw from './Draw'
+
+export default {
+  components: {Draw},
+  data() {
+    return {
+      pointList: []
+    }
+  },
+  methods: {
+    handleAdd(point){
+      console.log(111)
+      this.pointList.push(point)
+    },
+    pointClick(){
+      console.log(222)
+    }
+  }
+}
+</script>
+<style lang="less">
+
+</style>
---
+++ ImageMark.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="image-mark">
+    <img src="http://static.doveaz.xyz/img/2d.jpg" alt="" @click="handleBoardClick" />
+    <ul class="point-list">
+      <li
+        v-for="point in pointList"
+        :key="point.id"
+        :class="point.class"
+        :style="{ top: point.y + '%', left: point.x + '%', ...pointStyle, backgroundColor: point.color }"
+        @click="handlePointClick(point)"
+      >
+        {{ point.id }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ImageMark',
+  props: {
+    pointList: {
+      type: Array,
+      required: true
+    },
+    pointStyle: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    handleBoardClick(e) {
+      let point = {}
+      if (uni) {
+        const query = uni.createSelectorQuery().in(this)
+        query
+          .select('.draw')
+          .boundingClientRect(el => {
+            console.log(el)
+            console.log(e)
+            point = {
+              x: ((e.detail.x - el.left) / el.width) * 100,
+              y: ((e.detail.y - el.top) / el.height) * 100,
+              id: this.pointList.length + 1
+            }
+            this.$emit('add', point)
+          })
+          .exec()
+      } else {
+        point = {
+          x: (e.layerX / e.target.width) * 100,
+          y: (e.layerY / e.target.height) * 100,
+          id: this.pointList.length + 1
+        }
+        this.$emit('add', point)
+      }
+    },
+    handlePointClick(point) {
+      this.$emit('pointClick', point)
+    }
+  }
+}
+</script>
+<style lang="less">
+.image-mark {
+  position: relative;
+  border: 1px solid #f006;
+  width: 100%;
+
+  img {
+    width: 100%;
+  }
+
+  .point-list {
+    list-style: none;
+    margin: 0;
+
+    li {
+      z-index: 100;
+      user-select: NONE;
+      position: absolute;
+      /* prettier-ignore */
+      width: 26PX;
+      /* prettier-ignore */
+      height: 26PX;
+      /* prettier-ignore */
+      line-height: 26PX;
+      background-color: #d7d7d7;
+      border-radius: 50%;
+      color: #fff;
+      text-align: center;
+      transform: translate(-50%, -50%);
+    }
+  }
+}
+</style>
